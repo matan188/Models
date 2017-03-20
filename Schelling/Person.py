c1c056475bd9aa942d95fdbbc4e5162d8477390d
Matan <matan.modai@gmail.com>
2017-03-20 09:37:50 +0200

    Schelling: Change person satisfaction to be in its class. Change logic to calculate neighbors

--- Schelling/Person.py
+++ Schelling/Person.py
@@ -1,8 +1,9 @@
-
+import random
 class Person:
     def __init__(self, race, threshold=100):
         self._race = race
         self._threshold = threshold
+        self._timer = random.randint(2, 6)
 
     def __str__(self):
         return str(self._race)
@@ -19,5 +20,17 @@ class Person:
     def equal_race(self, race):
         return race == self._race
 
+    def is_satisfied(self, perc_same_neighbors):
+        """ Returns True if person is happy with where it lives else False """
+        time_satisfaction = False
+        if self._timer < 0:
+            self._timer = random.randint(1, 6)
+        else:
+            self._timer -= 1
+            time_satisfaction = True
+        # return time_satisfaction and perc_same_neighbors >= self.get_threshold()
+        return perc_same_neighbors >= self.get_threshold()
+
     def __float__(self):
+        """ Implementation of float casting for plotting """
         return float(self._race)
--- Schelling/Town.py
+++ Schelling/Town.py
@@ -11,6 +11,7 @@ NUM_OF_NEIGHBORS = 8.0
 class Town:
 
     def __init__(self, dimension=30, pop_ratios=(0.45, 0.45), thresholds=(0.5, 0.5)):
+        """ Initialize town object """
         self._dimension = dimension
         self._size = dimension*dimension
         self._thresholds = thresholds
@@ -18,9 +19,13 @@ class Town:
         self._empty_coords = self.get_coords(-1)
 
     def get_person_at_coord(self, row, col):
+        """ Returns the person at coordinate (row,col) """
+        if row < 0 or row >= self._dimension or col < 0 or col >= self._dimension:
+            return None
         return self._grid[row][col]
 
     def get_coords(self, race):
+        """ Returns an array with all the coordinates of people of race race """
         coords_arr = []
         for row in range(self._dimension):
             for col in range(self._dimension):
@@ -29,6 +34,7 @@ class Town:
         return coords_arr
 
     def create_new_town(self, size, pop_ratios):
+        """ Creates a new town with population placed randomly according to ratios """
         race_nums = [math.floor(ratio * (size)) for ratio in pop_ratios]
         empties = self._size - sum(race_nums)
         self._empties = empties
@@ -44,72 +50,57 @@ class Town:
         grid = np.array(grid)
         return grid.reshape([self._dimension, self._dimension])
 
-    # Check if person is satisfied of its neighbors
     def check_neighbors_satisfaction(self, person, row, col):
+        """ Check if person is satisfied of its neighbors """
         if self.get_person_at_coord(row, col).get_race() == -1:
             return True
-        return (self.same_neighbors_perc(person, row, col)) >= person.get_threshold()
+        return person.is_satisfied(self.same_neighbors_perc(row, col))
 
-    def same_neighbors_perc(self, person, row, col):
-        pers_race = person.get_race()
+    def same_neighbors_perc(self, row, col):
+        """ Returns the percentage of neighbors that are of same race as person """
+        pers_race = self.get_person_at_coord(row, col).get_race()
 
         neighbors_num = 0
         same_race_counter = 0
-        empty_neighbors = 0
 
-        if row - 1 >= 0:
-            if self.get_person_at_coord(row-1, col).get_race() != -1:
-                neighbors_num += 1
-            same_race_counter += (self.get_person_at_coord(row-1, col).equal_race(pers_race))
-            if col + 1 < self._dimension:
-                if self.get_person_at_coord(row-1, col+1).get_race() != -1:
-                    neighbors_num += 1
-                same_race_counter += (self.get_person_at_coord(row-1, col+1).equal_race(pers_race))
-            if col - 1 >= 0:
-                if self.get_person_at_coord(row-1, col-1).get_race() != -1:
-                    neighbors_num += 1
-                same_race_counter += (self.get_person_at_coord(row-1, col-1).equal_race(pers_race))
-        if row + 1 < self._dimension:
-            if self.get_person_at_coord(row+1, col).get_race() != -1:
-                neighbors_num += 1
-            same_race_counter += (self.get_person_at_coord(row+1, col).equal_race(pers_race))
-            if col + 1 < self._dimension:
-                if self.get_person_at_coord(row+1, col+1).get_race() != -1:
-                    neighbors_num += 1
-                same_race_counter += (self.get_person_at_coord(row+1, col+1).equal_race(pers_race))
-            if col - 1 >= 0:
-                if self.get_person_at_coord(row+1, col-1).get_race() != -1:
-                    neighbors_num += 1
-                same_race_counter += (self.get_person_at_coord(row+1, col-1).equal_race(pers_race))
-        if col + 1 < self._dimension:
-            if self.get_person_at_coord(row, col+1).get_race() != -1:
-                neighbors_num += 1
-            same_race_counter += (self.get_person_at_coord(row, col+1).equal_race(pers_race))
-        if col - 1 >= 0:
-            if self.get_person_at_coord(row, col-1).get_race() != -1:
-                neighbors_num += 1
-            same_race_counter += (self.get_person_at_coord(row, col-1).equal_race(pers_race))
+        for prev_row in range(row - 1, row + 2):
+            for prev_col in range(col-1, col + 2):
+                if prev_row != row or prev_col != col:
+                    curr = self.get_person_at_coord(prev_row, prev_col)
+                    if curr == None:
+                        continue
+                    same_race_counter += 1 if curr.equal_race(pers_race) else 0
+                    neighbors_num += 1 if curr.get_race() != -1 else 0
 
-        if neighbors_num == 0:
+        if neighbors_num == 0:  # If no neighbors, person is happy
             return 1
         return same_race_counter / float(neighbors_num)
 
-
     def run_cycle(self):
+        """ A cycle means going through all the population and transfer to random empty space if needed """
+        someone_transferred = False
         for row in range(self._dimension):
             for col in range(self._dimension):
                 if not self.check_neighbors_satisfaction(self.get_person_at_coord(row, col), row, col):
                     self.transfer(row, col)
+                    someone_transferred = True
+        return someone_transferred
 
     def run_n_cycles(self, n=10):
+        """ Run n cycles or until convergence """
+        cycles = 0
         for cycle in range(n):
-            self.run_cycle()
-
+            cycles += 1
+            if not self.run_cycle():
+                return cycles
+        return cycles
 
     def set_person_to(self, person, row, col):
+        """ Set person to coordinate """
         self._grid[row][col] = person
 
     def transfer(self, row, col):
+        """ Swap person with random empty space using the empty spaces coordinates array """
         rand_int = random.randint(0, len(self._empty_coords)-1)
         new_coords_ind = rand_int
         empty_row, empty_col = self._empty_coords[new_coords_ind]
@@ -122,13 +113,14 @@ class Town:
 
         self._empty_coords[new_coords_ind] = (row, col)
 
-
     def display(self):
+        """ Display town map """
         to_show = [[float(self.get_person_at_coord(row, col)) for col in range(self._dimension)] for row in range(self._dimension)]
         cmap = colors.ListedColormap(['gray', 'yellow', 'blue'])
         plt.imshow(to_show, cmap=cmap, interpolation="nearest", extent=[0, self._dimension, 0, self._dimension])
         plt.show()
 
+    # Plot town map
     def plot(self):
         to_show = [[float(self.get_person_at_coord(row, col)) for col in range(self._dimension)] for row in
                    range(self._dimension)]
@@ -136,22 +128,24 @@ class Town:
         print(cmap)
         plt.imshow(to_show, cmap=cmap, interpolation="nearest", extent=[0, self._dimension, 0, self._dimension])
 
+    # Show plot
     def show(self):
         plt.show()
 
+    # Calculate town's segregation level
     def segregation_level(self):
         segregated_persons = 0
         for row in range(self._dimension):
             for col in range(self._dimension):
-                if self.same_neighbors_perc(self.get_person_at_coord(row, col), row, col) == 1:
+                if self.same_neighbors_perc(row, col) == 1:
                     segregated_persons += 1
-        return segregated_persons / float(self._size - self._empties)
+        return segregated_persons / float(self._size - self._empties), segregated_persons
 
 
-town = Town(dimension=5)
+town = Town(dimension=6)
 # print(town._grid)
 print(town.segregation_level())
 town.display()
-town.run_n_cycles()
+print(town.run_n_cycles(30))
 print(town.segregation_level())
 town.display()
