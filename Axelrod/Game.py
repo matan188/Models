db45698cc35bfee46e61839be25c04809027eeff
Matan <matan.modai@gmail.com>
2017-04-08 19:04:07 +0300

    Axelrod: need to finish new generation, check the numbers of the new generation and check the mutations

--- Axelrod/Game.py
+++ Axelrod/Game.py
@@ -6,6 +6,7 @@ TEMPTATION = 3
 PUNISHMENT = -9
 HURT = -1
 ENFORCEMENT = -2
+METAPUNISHMENT = 0
 
 class Game:
     # TODO change default to 40
@@ -15,22 +16,34 @@ class Game:
         self.P = PUNISHMENT
         self.H = HURT
         self.E = ENFORCEMENT
+        self.M = 0
 
     def run_round(self):
-        seen_probability = rand.random()
+        """ Goes through each player, checks if defects and activate other players accordingly """
+        s = rand.random()
         for player in self._players:
-            if player.get_boldness() > seen_probability:
+            if player.boldness_probability() > s:
                 player.update_score(TEMPTATION)
                 got_punished = False
                 for other in self._players:
                     if other != player:
                         other.update_score(self.H)
-                        if rand.random() < seen_probability:
+                        if rand.random() < s:
                             if other.is_punish():
                                 if not got_punished:
                                     got_punished = True
                                     player.update_score(PUNISHMENT)
                                 other.update_score(ENFORCEMENT)
+                            elif self.M != 0:
+                                got_meta = False
+                                for meta_punisher in self._players:
+                                    if meta_punisher != other and meta_punisher != player:
+                                        if meta_punisher.is_punish():
+                                            if got_meta:
+                                                other.update_score(self.M)
+                                                got_meta = True
+                                            meta_punisher.update_score(self.E)
+
 
     def new_generation(self):
         scores = np.array([player.get_score() for player in self._players])
@@ -39,7 +52,12 @@ class Game:
         new_players = []
         for player in self._players:
             if player.get_score() > average + std:
-                new_players.append(Player(player.get_boldness_level(), player.get_vengefulness_level()))
+                for i in range(2):
+                    new_players.append(Player(player._boldness_binary, player._vengefulness_binary))
+            elif player.get_score() > average - std and player.get_score() < average + std:
+                new_players.append(Player(player._boldness_binary, player._vengefulness_binary))
+
+        self._players = new_players[:20]
 
     def print_boldness(self):
         print([player.get_boldness_level() for player in self._players])
@@ -50,11 +68,15 @@ class Game:
     def print_score(self):
         print([player.get_score() for player in self._players])
 
+    def split_to_bins(self):
+        pass
+
 game = Game()
 game.print_boldness()
 game.print_vengeance()
 game.print_score()
 game.run_round()
 game.print_score()
-# game.new_generation()
-
+game.new_generation()
+print(len(game._players))
+game.print_boldness()
--- Axelrod/Player.py
+++ Axelrod/Player.py
@@ -1,13 +1,29 @@
 import random as rand
 
+MUTATION_PROB = 0.01
+
 class Player:
-    def __init__(self, bold_level=None, venge_level=None):
-        if bold_level == None:
+    def __init__(self, boldness=None, venge=None):
+        if boldness == None:
             self._boldness_binary = [rand.randint(0, 1), rand.randint(0, 1), rand.randint(0, 1)]
             self._vengefulness_binary = [rand.randint(0, 1), rand.randint(0, 1), rand.randint(0, 1)]
-            self._score = 0
         else:
-            pass
+            new_bold = []
+            new_venge = []
+            for i in range(len(boldness)):
+                randi = rand.random()
+                if randi < MUTATION_PROB:
+                    new_bold.append(1 - boldness[i])
+                else:
+                    new_bold.append(boldness[i])
+            for j in range(len(venge)):
+                if rand.random() < MUTATION_PROB:
+                    new_venge.append(1 - venge[j])
+                else:
+                    new_venge.append(venge[j])
+            self._boldness_binary = new_bold
+            self._vengefulness_binary = new_venge
+        self._score = 0
 
     def update_score(self, points):
         self._score += points
@@ -19,12 +35,12 @@ class Player:
             level += self._boldness_binary[i] * pow(2, length - i - 1)
         return level
 
-    def get_boldness(self):
+    def boldness_probability(self):
         return self.get_boldness_level() / 7.0
 
     def is_defect(self):
         bold_threshold = rand.random()
-        return self.get_boldness() > bold_threshold
+        return self.boldness_probability() > bold_threshold
 
     def get_vengefulness_level(self):
         level = 0
@@ -33,12 +49,12 @@ class Player:
             level += self._vengefulness_binary[i] * pow(2, length-i-1)
         return level
 
-    def get_vengefulness(self):
+    def vengeance_probability(self):
         return self.get_vengefulness_level() / 7.0
 
     def is_punish(self):
         veng_threshold = rand.random()
-        return self.get_vengefulness() > veng_threshold
+        return veng_threshold < self.vengeance_probability()
 
     def get_score(self):
         return self._score
