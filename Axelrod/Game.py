33ef76342b4326733326b4979883cfc1df1bcefd
Matan <matan.modai@gmail.com>
2017-04-12 12:01:47 +0300

    Axelrod: Arrows in right direction. Probability of being seen according to Renana's guidelinse. Documentation. Each population runs for n=4 rounds

--- Axelrod/Game.py
+++ Axelrod/Game.py
@@ -18,7 +18,7 @@ class Game:
         self.P = PUNISHMENT
         self.H = HURT
         self.E = ENFORCEMENT
-        self.M = 0
+        self.M = METAPUNISHMENT
         self.pop_stats = []
         self.bins = dict()
         self.post_bins = dict()
@@ -26,6 +26,7 @@ class Game:
         self.cycle_venge_averages = []
 
     def reinit_players(self):
+        """ Initialize a new list of player to start a new cycle\experiment """
         length = len(self._players)
         self._players = [Player() for i in range(length)]
 
@@ -39,7 +40,8 @@ class Game:
                 for other in self._players:
                     if other != player:
                         other.update_score(self.H)
-                        if rand.random() < s:
+                        # the chance of not getting caught is 1-s
+                        if rand.random() < 1-pow(1-s, 1/(len(self._players)-1)):
                             if other.is_punish():
                                 if not got_punished:
                                     got_punished = True
@@ -57,6 +59,7 @@ class Game:
 
 
     def new_generation(self):
+        """ Creates a new generation based on previous generation's players' scores """
         scores = np.array([player.get_score() for player in self._players])
         average = scores.mean()
         std = scores.std()
@@ -68,14 +71,14 @@ class Game:
                 bests.append(player)
                 for i in range(2):
                     new_players.append(Player(player._boldness_binary, player._vengefulness_binary))
-            elif player.get_score() > average - std and player.get_score() < average + std:
+            elif player.get_score() >= average - std and player.get_score() <= average + std:
                 medium.append(player)
                 new_players.append(Player(player._boldness_binary, player._vengefulness_binary))
 
-        if len(new_players)<20:
+        if len(new_players) < 20:
             for player in bests:
                 new_players.append(Player(player._boldness_binary, player._vengefulness_binary))
-        if len(new_players)<20:
+        if len(new_players) < 20:
             for player in medium:
                 new_players.append(Player(player._boldness_binary, player._vengefulness_binary))
         if len(new_players) == 0:
@@ -87,24 +90,35 @@ class Game:
         for p in self._players:
             p.init_score()
 
-    def update_initial_stats(self):
-        boldness_avg = np.mean([player.get_boldness_level() for player in self._players])
-        venge_avg = np.mean([player.get_vengefulness_level() for player in self._players])
-        self.pop_stats.append((boldness_avg, venge_avg))
-
     def run_n_generations(self, n=100):
+        """ Runs n generations, each for a number of rounds according to run_n_rounds function.
+            The function also updates the stats (averages) of each generation and creates new generation
+            according to the scores after the rounds. """
         for curr_gen in range(n):
             self.update_initial_stats()
-            self.run_round()
+            self.run_n_rounds()
             self.new_generation()
 
+    def run_n_rounds(self, n=4):
+        """ Default is 4 rounds so each player has 4 opportunities to defect """
+        for i in range(n):
+            self.run_round()
+
     def run(self, times=5, generations=100):
+        """ Runs times cycles of experiments. each cycle comprises n generations.  """
         for i in range(times):
             self.run_n_generations(generations)
             self.update_cycle_averages()
             self.reinit_players()
 
+    def update_initial_stats(self):
+        """ Updates the averages of the current population """
+        boldness_avg = np.mean([player.get_boldness_level() for player in self._players])
+        venge_avg = np.mean([player.get_vengefulness_level() for player in self._players])
+        self.pop_stats.append((boldness_avg, venge_avg))
+
     def update_cycle_averages(self):
+        """ Averages of population at the end of generation. This way we can see if a norm was created """
         bold_avg = np.mean([player.get_boldness_level() for player in self._players])
         venge_avg = np.mean([player.get_vengefulness_level() for player in self._players])
         self.cycle_bold_averages.append(bold_avg)
@@ -120,13 +134,17 @@ class Game:
         print([player.get_score() for player in self._players])
 
     def pop_to_bins(self):
+        """ Divides all populations stats into bins. The bins are divided according to
+            average boldness and vengefulness. Bin i,j will include the populations who's averages were
+            i<=boldness<i+1 j<=vengefulness<j+1 """
         for ind in range(len(self.pop_stats)):
             if (ind+1) % 100 == 0:  # ignore when starting new round of generations
                 continue
             stat = self.pop_stats[ind]
-            post_stat =  self.pop_stats[ind+1]
-            b = math.floor(stat[0]) if math.floor(stat[0]) < 7 else 6
-            v = math.floor(stat[1]) if math.floor(stat[1]) < 7 else 6
+            post_stat = self.pop_stats[ind+1]
+
+            b = math.floor(stat[0]) #math.floor(stat[0]) if math.floor(stat[0]) < 7 else 6
+            v = math.floor(stat[1]) #math.floor(stat[1]) if math.floor(stat[1]) < 7 else 6
             if (b, v) in self.bins:
                 self.bins[(b, v)].append(stat)
                 self.post_bins[(b, v)].append(post_stat)
@@ -135,6 +153,7 @@ class Game:
                 self.post_bins[(b, v)] = [post_stat]
 
     def bins_to_arrow_tuples(self):
+        """ Prepare bins to be represented as arrows in the graph. """
         arrows = []
         for key in self.bins:
             bold = []
@@ -148,22 +167,23 @@ class Game:
                 venge_next.append(self.post_bins[key][ind][1])
             b = np.mean(bold)
             v = np.mean(venge)
-            db = b-np.mean(bold_next)
-            dv = v-np.mean(venge_next)
+            db = np.mean(bold_next)-b
+            dv = np.mean(venge_next)-v
             arrow = (b, v, db, dv)
             arrows.append(arrow)
         return arrows
 
     def display(self):
         arrows = self.bins_to_arrow_tuples()
-        # x = np.arange(8)
-        # y = x
-        # plt.plot(x, y)
         ax = plt.gca()
         for arr in arrows:
             patch = plt.Arrow(arr[0], arr[1], arr[2], arr[3], width=0.3)
             ax.add_patch(patch)
         plt.scatter(self.cycle_bold_averages, self.cycle_venge_averages, edgecolors='black')
+        plt.xlabel("Boldness Level")
+        plt.ylabel("Vengefulness Level")
+        plt.ylim((0, 7))
+        plt.xlim((0, 7))
         plt.show()
 
 
