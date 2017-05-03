17d6ce071e79c58fc6aa5ee2c75ef8b004452408
Matan <matan.modai@gmail.com>
2017-05-03 13:05:05 +0300

    Epstein: with 2 agents types

--- Epstein/Agent.py
+++ Epstein/Agent.py
@@ -2,25 +2,27 @@ import random
 import numpy as np
 
 MOVES = [30, 50, 70]
-EPSILON = 0.2
+EPSILON = 0.1
 
 class Agent:
     def __init__(self, m=10, epsilon=EPSILON, tag=0):
-        self.memory = [random.choice(MOVES) for i in range(10)]
+        self.memory = []
+        self.memory.append([30 if tag==1 else 70 for i in range(10)])
+        self.memory.append([70 if tag==1 else 30 for i in range(10)]) #random.choice(MOVES)
         self.m = m
         self.epsilon = epsilon
         self.tag = tag
 
-    def update_memory(self, move):
-        self.memory.pop(0)
-        self.memory.append(move)
+    def update_memory(self, move, tag=0):
+        self.memory[tag].pop(0)
+        self.memory[tag].append(move)
 
-    def best_move(self):
+    def best_move(self, tag=0):
         best_moves = []
         curr_max = 0
         for move_ind in range(len(MOVES)):
             move = MOVES[move_ind]
-            outcome = sum([0 if (move+past)>100 else move for past in self.memory])
+            outcome = sum([0 if (move+past)>100 else move for past in self.memory[tag]])
             if outcome == curr_max:
                 best_moves.append(move)
             elif outcome > curr_max:
@@ -35,22 +37,21 @@ class Agent:
             gains.append(sum([0 if (move+past)>100 else move for past in self.memory]))
         return gains
 
-    def get_history_percentage(self):
+    def get_tag(self):
+        return self.tag
+
+    def get_history_percentage(self, tag=0):
         """ Returns a dictionary with history percentages """
         dic = dict()
-        dic["Low"] = self.memory.count(MOVES[0])/10.0
-        dic["Medium"] = self.memory.count(MOVES[1])/10.0
-        dic["High"] = self.memory.count(MOVES[2])/10.0
-        dic["label"] = "Agent"
+        dic["Low"] = self.memory[tag].count(MOVES[0])/10.0
+        dic["Medium"] = self.memory[tag].count(MOVES[1])/10.0
+        dic["High"] = self.memory[tag].count(MOVES[2])/10.0
+        dic["label"] = "Agent {}".format(self.tag)
         return dic
 
-
-    def play(self, opponent=None):
-        if opponent is None:
-            # Epsilon probability
-            if random.random() < self.epsilon:
-                return random.choice(MOVES)
-            # Else play according to memory
-            return self.best_move()
-        else:
-            pass
+    def play(self, tag=0):
+        # Epsilon probability
+        if random.random() < self.epsilon:
+            return random.choice(MOVES)
+        # Else play according to memory
+        return self.best_move(tag=tag)
--- Epstein/Framework.py
+++ Epstein/Framework.py
@@ -2,13 +2,13 @@ import plotly.plotly as py
 import plotly.graph_objs as go
 import plotly
 import plotly.figure_factory as ff
-from Epstein.Agent import  *
+from Epstein.Agent import *
 
 
 class Framework:
     def __init__(self, num_agents=200.0):
         self.num_agents = num_agents
-        self.agents = [Agent() for ag in range(int(num_agents))]
+        self.agents = [Agent(tag=ag%2) for ag in range(int(num_agents))]
         self.move_percentages = []
 
     def round(self):
@@ -20,20 +20,20 @@ class Framework:
             p2 = self.agents.pop(idx2)
             reagents.append(p1)
             reagents.append(p2)
-            move1 = p1.play()
-            move2 = p2.play()
-            p1.update_memory(move2)
-            p2.update_memory(move1)
+            move1 = p1.play(tag=p2.get_tag())
+            move2 = p2.play(tag=p1.get_tag())
+            p1.update_memory(move2, tag=p2.get_tag())
+            p2.update_memory(move1, tag=p1.get_tag())
         self.agents = reagents
 
     def update_percentages(self):
         self.move_percentages.append(self.get_moves_percentages())
 
-    def get_moves_percentages(self):
+    def get_moves_percentages(self, tag=0):
         moves_track = dict({30: 0, 50: 0, 70: 0})
 
         for ag in self.agents:
-            moves_track[ag.best_move()] += 1
+            moves_track[ag.best_move(tag=tag)] += 1
         tup = (moves_track[30] / self.num_agents, moves_track[50] / self.num_agents, moves_track[70] / self.num_agents)
         return tup
 
@@ -41,33 +41,36 @@ class Framework:
         moves_track = dict({30: 0, 50: 0, 70: 0})
 
         for ag in self.agents:
-            for mem in ag.memory:
+            for mem in ag.memory[0]:
                 moves_track[mem] += 1
         tup = (moves_track[30] / (self.num_agents*10), moves_track[50] / (self.num_agents*10),
                moves_track[70] / (self.num_agents*10))
         return tup
 
     def run_n_rounds(self, n=100):
-        print("memory percentages:",self.get_memory_percentages())
+        print("memory percentages:", self.get_memory_percentages())
         self.update_percentages()
         print("{}: {}".format(0, self.move_percentages[0]))
         for i in range(n):
             self.round()
             self.update_percentages()
             percentages = self.move_percentages[-1]
-            if percentages[0] > percentages[1] or percentages[2] > percentages[1]:
-                print("Fractious")
-                print(percentages)
-                self.display_players()
+            # if percentages[0] > percentages[1] or percentages[2] > percentages[1]:
+            # if i%10==0:
+            #     print("Fractious")
+            #     print(percentages)
+            #     self.display_players()
         print("{}: {}".format(i+1, self.move_percentages[i]))
 
     def run_simulations(self, num=20):
-        pass
+        for i in range(num):
+            self.run_n_rounds()
+            self.agents = [Agent() for ind in range(int(self.num_agents))]
 
-    def display_players(self):
+    def display_players(self, oppsit):
         raw_data = []
         for agent in self.agents:
-            raw_data.append(agent.get_history_percentage())
+            raw_data.append(agent.get_history_percentage(tag=abs(oppsit-agent.get_tag())))
 
         data = [{
             'type': 'scatterternary',
@@ -124,20 +127,47 @@ class Framework:
         table = ff.create_table(table_data)
         py.plot(table)
 
+    def get_agent_type_percentages(self, self_tag, opponent_tag):
+        percentages = dict({30: 0, 50: 0, 70: 0})
+        for agent in self.agents:
+            if agent.get_tag() == self_tag:
+                for mem in agent.memory[opponent_tag]:
+                    percentages[mem] += 1
+        tup = (percentages[30] / ((self.num_agents/2) * 10),
+               percentages[50] / ((self.num_agents/2) * 10),
+               percentages[70] / ((self.num_agents/2) * 10))
+        return tup
 
-# fram = Framework(num_agents=100)
+    def print_agents_stats(self):
+        print("0 to 0:", fram.get_agent_type_percentages(0, 0))
+        print("0 to 1:", fram.get_agent_type_percentages(0, 1))
+        print("1 to 0:", fram.get_agent_type_percentages(1, 0))
+        print("1 to 1:", fram.get_agent_type_percentages(1, 1))
+
+
+
+
+
+fram = Framework(num_agents=200)
+fram.print_agents_stats()
+fram.run_n_rounds()
+fram.print_agents_stats()
+
+fram.display_players(oppsit=0)
+fram.display_players(oppsit=1)
 # print(fram.get_memory_percentages())
 # print(fram.get_moves_percentages())
 # fram.display_first_table()
 # fram.display_players()
-# fram.run_n_rounds(20)
-# print(fram.move_percentages[-1])
+# fram.run_n_rounds(100)
+# print("tag 0", fram.get_moves_percentages(tag=0))
+# print("tag 1", fram.get_moves_percentages(tag=1))
 # fram.display_players()
 
 
-ag = Agent()
-print(ag.memory)
-print(ag.get_history_percentage())
-print(ag.get_move_gains())
+# ag = Agent()
+# print(ag.memory)
+# print(ag.get_history_percentage())
+# print(ag.get_move_gains())
 
 
